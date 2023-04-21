eabb1acde933fed91b7ec31e7f51b4b363300a0a
Jun Ono <108505323+NotJ11@users.noreply.github.com>
2023-04-20 23:46:28 -0300

    Add files via upload

---
+++ pmysql/utils.py
@@ -0,0 +1,134 @@
+import MySQLdb
+
+
+def conectar():
+    """
+    Função para conectar ao servidor
+    """
+    try:
+        conn = MySQLdb.connect(
+            db='pmysql',
+            host='localhost',
+            user='#username',
+            passwd= '#password'
+        )
+        return conn
+    except MySQLdb.Error as e:
+        print(f'Erro na conexão ao MySQL server: {e}')
+
+def desconectar(conn):
+    """ 
+    Função para desconectar do servidor.
+    """
+    if conn:
+        conn.close()
+
+
+def listar():
+    """
+    Função para listar os produtos
+    """
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM produtos')
+    produtos = cursor.fetchall()
+
+    if len(produtos) > 0:
+        print('Listando produtos..')
+        print('-----------------------')
+        for produto in produtos:
+            print(f'ID: {produto[0]}')
+            print(f'Produto: {produto[1]}')
+            print(f'Preço: {produto[2]}')
+            print(f'Estoque: {produto[3]}')
+            print('-----------------------')
+    else:
+        print(f'Não exite produto cadastrado')
+
+
+def inserir():
+    """
+    Função para inserir um produto
+    """  
+    conn = conectar()
+    cursor = conn.cursor()
+
+    nome = input('Informe o nome do produto: ')
+    preco = float(input('Informe o preço do produto: '))
+    estoque = int(input('Informe a quantidade em estoque: '))
+
+    cursor.execute(f"INSERT INTO produtos (nome, preco, estoque) VALUE ('{nome}', {preco}, {estoque} )")
+    conn.commit()
+
+    if cursor.rowcount == 1:
+        print(f'O produto {nome} foi inserida com sucesso')
+    else:
+        print(f'Não foi possivel inserir o produto.')
+    desconectar(conn)
+
+
+def atualizar():
+    """
+    Função para atualizar um produto
+    """
+    conn = conectar()
+    cursor = conn.cursor()
+
+    codigo = int(input("Informe o código do produto: "))
+    nome = input("Informe o novo nome do produto: ")
+    preco = float(input("Informe o novo preço do produto: "))
+    estoque = int(input("Informe a nova quantidade em estoque: "))
+
+    cursor.execute(f"UPDATE produtos SET nome='{nome}', preco={preco}, estoque={estoque} WHERE id={codigo}")
+    conn.commit()
+
+    if cursor.rowcount == 1:
+        print(f'O produto foi atualizado com sucesso')
+    else: 
+        print(f'Erroa o atualizar o produto')
+    desconectar(conn)
+
+
+def deletar():
+    """
+    Função para deletar um produto
+    """  
+    conn = conectar()
+    cursor = conn.cursor()
+
+    codigo = int(input('Informe o código do produto: '))
+
+    cursor.execute(f"DELETE FROM produtos WHERE id={codigo}")
+    conn.commit()
+
+    if cursor.rowcount == 1:
+        print('Produto excluido com sucesso')
+    else:
+        print(f'erro ao excluir o produto com id = {codigo}')
+    desconectar(conn)
+
+
+def menu():
+    """
+    Função para gerar o menu inicial
+    """
+    print('=========Gerenciamento de Produtos==============')
+    print('Selecione uma opção: ')
+    print('1 - Listar produtos.')
+    print('2 - Inserir produtos.')
+    print('3 - Atualizar produto.')
+    print('4 - Deletar produto.')
+    opcao = int(input())
+    if opcao in [1, 2, 3, 4]:
+        if opcao == 1:
+            listar()
+        elif opcao == 2:
+            inserir()
+        elif opcao == 3:
+            atualizar()
+        elif opcao == 4:
+            deletar()
+        else:
+            print('Opção inválida')
+    else:
+        print('Opção inválida')
